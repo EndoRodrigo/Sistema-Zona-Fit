0d22412cdabcaa83a610190e2113be31987bdbb9
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-02-02 19:25:36 -0500

    feat: creacion de la estrucuta del proyecto

---
+++ src/main/java/com/endorodrigo/Sistema/Zona/Fit/SistemaZonaFitApplication.java
@@ -0,0 +1,13 @@
+package com.endorodrigo.Sistema.Zona.Fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaZonaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaZonaFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endorodrigo/Sistema/Zona/Fit/SistemaZonaFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endorodrigo.Sistema.Zona.Fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaZonaFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
