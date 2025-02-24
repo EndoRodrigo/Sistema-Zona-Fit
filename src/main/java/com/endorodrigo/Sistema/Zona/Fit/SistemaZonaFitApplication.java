a49bc227fa8f0b5ba51abee8a2dfefbd924022e9
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-02-23 21:29:55 -0500

    feat: se agrega interface UX Swing

--- src/main/java/com/endorodrigo/Sistema/Zona/Fit/SistemaZonaFitApplication.java
+++ src/main/java/com/endorodrigo/Sistema/Zona/Fit/SistemaZonaFitApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Scanner;
 
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SistemaZonaFitApplication implements CommandLineRunner {
 
 	@Autowired
